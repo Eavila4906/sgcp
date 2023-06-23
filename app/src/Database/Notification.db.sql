a78d31cdd3ee573cb4ca4f063b56fe73244cac22
Eavila4906 <eavila4906@gmail.com>
2023-06-23 14:46:26 -0500

    Implementation of new fields

--- app/src/Database/Notification.db.sql
+++ app/src/Database/Notification.db.sql
@@ -7,13 +7,17 @@ CREATE TABLE `notification` (
   PRIMARY KEY (`id_notification`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `details_notification`;
-CREATE TABLE `details_notification` (
-  `id_details_notification` int(11) NOT NULL AUTO_INCREMENT,
-  `user` int(11) NOT NULL,
+DROP TABLE IF EXISTS `notification_details`;
+CREATE TABLE `notification_details` (
+  `id_notification_details` int(11) NOT NULL AUTO_INCREMENT,
+  `sending_user` int(11) NOT NULL,
+  `recipient_user` int(11) NOT NULL,
   `notification` int(11) NOT NULL,
+  `appointment` int(11) NOT NULL,
   `date` timestamp DEFAULT current_timestamp(),
-  PRIMARY KEY (`id_details_notification`),
-  FOREIGN KEY (`user`) REFERENCES `user` (`id_user`),
-  FOREIGN KEY (`notification`) REFERENCES `notification` (`id_notification`)
+  PRIMARY KEY (`id_notification_details`),
+  FOREIGN KEY (`sending_user`) REFERENCES `user` (`id_user`),
+  FOREIGN KEY (`recipient_user`) REFERENCES `user` (`id_user`),
+  FOREIGN KEY (`notification`) REFERENCES `notification` (`id_notification`),
+  FOREIGN KEY (`appointment`) REFERENCES `appointment` (`id_appointment`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
