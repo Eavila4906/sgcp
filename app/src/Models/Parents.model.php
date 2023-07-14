dc82cab85af31b213a27a9bfa04a9629917f1a6a
Eavila4906 <eavila4906@gmail.com>
2023-07-14 12:57:51 -0500

    Added representative control and added cell_phone2 field as secondary cell in all controller features

--- app/src/Models/Parents.model.php
+++ app/src/Models/Parents.model.php
@@ -28,12 +28,14 @@
         }
 
         public function create($user, $parentsData) {
+            $repr = $parentsData['repr'];
             $father_name = $parentsData['father_name'];
             $father_lastname = $parentsData['father_lastname'];
             $mother_name = $parentsData['mother_name'];
             $mother_lastname = $parentsData['mother_lastname'];
             $home_phone = $parentsData['home_phone'];
             $cell_phone = $parentsData['cell_phone'];
+            $cell_phone2 = $parentsData['cell_phone2'];
             $home_address = $parentsData['home_address'];
             $status = $parentsData['status'];
 
@@ -45,11 +47,11 @@
             if (!empty($req_validate)) {
                 $res = 'exists';
             } else {
-                $Query = "INSERT INTO parents (user, father_name, father_lastname, mother_name, mother_lastname, home_phone, cell_phone, home_address, status) 
-                          VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                $Query = "INSERT INTO parents (user, repr, father_name, father_lastname, mother_name, mother_lastname, home_phone, cell_phone, cell_phone2, home_address, status) 
+                          VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
                 $Array = array(
-                    $user, $father_name, $father_lastname, $mother_name, $mother_lastname, 
-                    $home_phone, $cell_phone, $home_address, $status
+                    $user, $repr, $father_name, $father_lastname, $mother_name, $mother_lastname, 
+                    $home_phone, $cell_phone, $cell_phone2, $home_address, $status
                 );
                 $req = $this->InsertMySQL($Query, $Array);
                 $req ?  $res = $req : $res = 0;
@@ -59,12 +61,17 @@
 
         // Patient update function
         public function update($id_parents, $parentsData) {
+            $name = $parentsData['name'];
+            $lastname = $parentsData['lastname'];
+            $repr = $parentsData['repr'];
             $father_name = $parentsData['father_name'];
             $father_lastname = $parentsData['father_lastname'];
             $mother_name = $parentsData['mother_name'];
             $mother_lastname = $parentsData['mother_lastname'];
+            $email = $parentsData['email'];
             $home_phone = $parentsData['home_phone'];
             $cell_phone = $parentsData['cell_phone'];
+            $cell_phone2 = $parentsData['cell_phone2'];
             $home_address = $parentsData['home_address'];
             $status = $parentsData['status'];
 
@@ -77,11 +84,15 @@
             if (!empty($req_validate)) {
                 $res = 'exists';
             } else {
-                $Query = "UPDATE parents SET father_name=?, father_lastname=?, mother_name=?, mother_lastname=?,
-                                 home_phone=?, cell_phone=?, home_address=?, status=? WHERE id_parents = $id_parents";
+                $Query = "UPDATE parents ps INNER JOIN user us ON (ps.user = us.id_user)
+                                 SET us.name=?, us.lastname=?, us.email=?, ps.repr=?,
+                                 ps.father_name=?, ps.father_lastname=?, ps.mother_name=?, ps.mother_lastname=?,
+                                 ps.home_phone=?, ps.cell_phone=?, ps.cell_phone2=?, ps.home_address=?, ps.status=? 
+                                 WHERE ps.id_parents = $id_parents";
                 $Array = array(
+                    $name, $lastname, $email, $repr,
                     $father_name, $father_lastname, $mother_name, $mother_lastname,
-                    $home_phone, $cell_phone, $home_address, $status
+                    $home_phone, $cell_phone, $cell_phone2, $home_address, $status
                 );
                 $req = $this->UpdateMySQL($Query, $Array);
                 $req ?  $res = 1 : $res = 0;
@@ -91,12 +102,18 @@
 
         // Parents delete function
         public function delete($id_parents, $status) {
-            $Query = "UPDATE parents ps INNER JOIN user us ON (ps.user=us.id_user)
-                             INNER JOIN patient pt ON (pt.parents=ps.id_parents)
-                      SET ps.status=?, us.status=?, pt.status=? WHERE id_parents = $id_parents";
-            $Array = array($status, $status, $status);
-            $req = $this->UpdateMySQL($Query, $Array);
-            $req ?  $res = 1 : $res = 0;
+            $Query_validate = "SELECT * FROM patient WHERE parents = '$id_parents' AND status != 0";
+            $req_validate = $this->SelectAllMySQL($Query_validate);
+
+            if (!empty($req_validate)) {
+                $res = "exists";
+            } else {
+                $Query = "UPDATE parents ps INNER JOIN user us ON (ps.user=us.id_user)
+                            SET ps.status=?, us.status=? WHERE id_parents = $id_parents";
+                $Array = array($status, $status);
+                $req = $this->UpdateMySQL($Query, $Array);
+                $req ?  $res = 1 : $res = 0;
+            }
             return $res;
         }
 
@@ -108,7 +125,14 @@
 
         // Unique parents get function
         public function get($id_parents) {
-            $Query = "SELECT us.username, us.email, ps.* FROM parents ps INNER JOIN user us ON (ps.user=us.id_user) WHERE id_parents = $id_parents";
+            $Query = "SELECT us.username, us.email, us.name, us.lastname, ps.*,
+                        CONCAT(us.name, ' ', us.lastname) AS representative,
+                        CONCAT(ps.father_name, ' ', ps.father_lastname) AS father, 
+                        CONCAT(ps.mother_name, ' ', ps.mother_lastname) AS mother
+                        FROM parents ps 
+                        INNER JOIN user us 
+                        ON (ps.user=us.id_user) 
+                        WHERE id_parents = $id_parents";
             return $this->SelectMySQL($Query);
         }
 
