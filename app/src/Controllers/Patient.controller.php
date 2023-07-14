c57fed57509599daf1d4a809b07658e5bfc6af11
Eavila4906 <eavila4906@gmail.com>
2023-07-14 13:00:27 -0500

    Observation flow control was added and the other functionalities for the patient module were completed

--- app/src/Controllers/Patient.controller.php
+++ app/src/Controllers/Patient.controller.php
@@ -14,20 +14,55 @@
                     $this->lastname = $_POST['lastname'];
                     $this->birthdate = $_POST['birthdate'];
                     $this->sex = $_POST['sex'];
+                    $this->weight_kg = $_POST['weight_kg'];
+                    $this->weight_pounds = $_POST['weight_pounds'];
+                    $this->height = $_POST['height'];
                     $this->blood_type = $_POST['blood_type'];
                     $this->family_obs = $_POST['family_obs'];
+                    $this->othersCheckOf = $_POST['othersCheckOf'] == false ? 0 : 1;
                     $this->personal_obs = $_POST['personal_obs'];
+                    $this->othersCheckOp = $_POST['othersCheckOp'] == false ? 0 : 1;
                     $this->general_obs = $_POST['general_obs'];
                     $this->status = $_POST['status'];
 
                     if (empty($this->dni) || empty($this->name) || empty($this->lastname) || empty($this->birthdate) 
-                        || empty($this->sex) || empty($this->blood_type) || empty($this->family_obs) || empty($this->personal_obs)
-                        || empty($this->general_obs) || empty($this->status)) {
+                        || empty($this->sex) || empty($this->weight_kg) || empty($this->weight_pounds) 
+                        || empty($this->height) || empty($this->blood_type)) 
+                    {
                         $res = array(
                             'status' => false, 
                             'msg' => 'All fields are required.'
                         );
                     } else {
+
+                        if ($this->othersCheckOf > 0) {
+                            if (empty($this->family_obs)) {
+                                $family_obs = $_POST['others_of'];
+                            } else {
+                                if (empty($_POST['others_of'])) {
+                                    $family_obs = $this->family_obs;
+                                } else {
+                                    $family_obs = $this->family_obs.', '.$_POST['others_of'];
+                                }
+                            }
+                        } else {
+                            $family_obs = $this->family_obs;
+                        }
+
+                        if ($this->othersCheckOp > 0) {
+                            if (empty($this->personal_obs)) {
+                                $personal_obs = $_POST['others_op'];
+                            } else {
+                                if (empty($_POST['others_op'])) {
+                                    $personal_obs = $this->personal_obs;
+                                } else {
+                                    $personal_obs = $this->personal_obs.', '.$_POST['others_op'];
+                                }
+                            }
+                        } else {
+                            $personal_obs = $this->personal_obs;
+                        }
+
                         $patientData = array(
                             'parents' => $this->id_parents,
                             'dni' => $this->dni,
@@ -35,12 +70,15 @@
                             'lastname' => $this->lastname,
                             'birthdate' => $this->birthdate,
                             'sex' => $this->sex,
+                            'weight_kg' => $this->weight_kg,
+                            'weight_pounds' => $this->weight_pounds,
+                            'height' => $this->height,
                             'blood_type' => $this->blood_type,
-                            'family_obs' => $this->family_obs,
-                            'personal_obs' => $this->personal_obs,
-                            'general_obs' => $this->general_obs,
+                            'family_obs' => $family_obs == '' ? 'Ninguna' : $family_obs,
+                            'personal_obs' => $personal_obs == '' ? 'Ninguna' : $personal_obs,
+                            'general_obs' => $this->general_obs == '' ? 'Ninguna' : $this->general_obs,
                             'status' => $this->status
-                        ); 
+                        );
                         $req = $this->model->create($patientData);
 
                         if ($req > 0) {
@@ -81,31 +119,67 @@
                     $this->lastname = $_POST['lastname'];
                     $this->birthdate = $_POST['birthdate'];
                     $this->sex = $_POST['sex'];
+                    $this->weight_kg = $_POST['weight_kg'];
+                    $this->weight_pounds = $_POST['weight_pounds'];
+                    $this->height = $_POST['height'];
                     $this->blood_type = $_POST['blood_type'];
                     $this->family_obs = $_POST['family_obs'];
+                    $this->othersCheckOf = $_POST['othersCheckOf'] == false ? 0 : 1;
                     $this->personal_obs = $_POST['personal_obs'];
+                    $this->othersCheckOp = $_POST['othersCheckOp'] == false ? 0 : 1;
                     $this->general_obs = $_POST['general_obs'];
                     $this->status = $_POST['status'];
 
                     if (empty($this->id_patient) || empty($this->dni) || empty($this->name) 
                         || empty($this->lastname) || empty($this->birthdate) || empty($this->sex)
-                        || empty($this->blood_type) || empty($this->family_obs) || empty($this->personal_obs)
-                        || empty($this->general_obs)) {
+                        || empty($this->blood_type)) {
                         $res = array(
                             'status' => false, 
                             'msg' => 'All fields are required.'
                         );
                     } else {
+
+                        if ($this->othersCheckOf > 0) {
+                            if (empty($this->family_obs)) {
+                                $family_obs = $_POST['others_of'];
+                            } else {
+                                if (empty($_POST['others_of'])) {
+                                    $family_obs = $this->family_obs;
+                                } else {
+                                    $family_obs = $this->family_obs.', '.$_POST['others_of'];
+                                }
+                            }
+                        } else {
+                            $family_obs = $this->family_obs;
+                        }
+
+                        if ($this->othersCheckOp > 0) {
+                            if (empty($this->personal_obs)) {
+                                $personal_obs = $_POST['others_op'];
+                            } else {
+                                if (empty($_POST['others_op'])) {
+                                    $personal_obs = $this->personal_obs;
+                                } else {
+                                    $personal_obs = $this->personal_obs.', '.$_POST['others_op'];
+                                }
+                            }
+                        } else {
+                            $personal_obs = $this->personal_obs;
+                        }
+
                         $patientData = array(
                             'dni' => $this->dni,
                             'name' => $this->name,
                             'lastname' => $this->lastname,
                             'birthdate' => $this->birthdate,
                             'sex' => $this->sex,
+                            'weight_kg' => $this->weight_kg,
+                            'weight_pounds' => $this->weight_pounds,
+                            'height' => $this->height,
                             'blood_type' => $this->blood_type,
-                            'family_obs' => $this->family_obs,
-                            'personal_obs' => $this->personal_obs,
-                            'general_obs' => $this->general_obs,
+                            'family_obs' => $family_obs == '' ? 'Ninguna' : $family_obs,
+                            'personal_obs' => $personal_obs == '' ? 'Ninguna' : $personal_obs,
+                            'general_obs' => $this->general_obs == '' ? 'Ninguna' : $this->general_obs,
                             'status' => $this->status
                         ); 
                         $req = $this->model->update($this->id_patient, $patientData);
@@ -156,6 +230,11 @@
                             $res = array(
                                 'status' => true, 
                                 'msg' => 'Patient deleted successfully.'
+                            );
+                        } else if ($req == "exists") {
+                            $res = array(
+                                'status' => false, 
+                                'msg' => 'This process could not be performed, You cannot delete a patient who has an active medical record.'
                             );
                         } else {
                             $res = array(
