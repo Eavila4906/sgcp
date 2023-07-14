e41eacae42f75f66bdfd040d1170e14e17ca1c7f
Eavila4906 <eavila4906@gmail.com>
2023-07-14 12:57:20 -0500

    Added representative control and added cell_phone2 field as secondary cell in all controller features

--- app/src/Controllers/Parents.controller.php
+++ app/src/Controllers/Parents.controller.php
@@ -8,12 +8,19 @@
         public function create() {
             if ($_POST) {
                 if (verifyApiKey()) {
+                    //Data personal
                     $this->father_name = $_POST['father_name'];
                     $this->father_lastname = $_POST['father_lastname'];
                     $this->mother_name = $_POST['mother_name'];
                     $this->mother_lastname = $_POST['mother_lastname'];
+                    //Representative
+                    $this->repr_check = $_POST['repr_check'];
+                    $this->repr_name = $_POST['repr_name'];
+                    $this->repr_lastname = $_POST['repr_lastname'];
+                    //Contacts
                     $this->home_phone = $_POST['home_phone'];
                     $this->cell_phone = $_POST['cell_phone'];
+                    $this->cell_phone2 = $_POST['cell_phone2'];
                     $this->email = $_POST['email'];
                     $this->home_address = $_POST['home_address'];
                     $this->rol = 5;
@@ -25,12 +32,23 @@
                             'msg' => 'All fields are required.'
                         );
                     } else {
-                        $this->username = usernameGenerator($this->mother_name, $this->mother_lastname, date("h:i:s"));
+                        if ($this->repr_check == 1) {
+                            $name = $this->father_name;
+                            $lastname = $this->father_lastname;
+                        } else if ($this->repr_check == 2) {
+                            $name = $this->mother_name;
+                            $lastname = $this->mother_lastname;
+                        } else if ($this->repr_check == 3) {
+                            $name = $this->repr_name;
+                            $lastname = $this->repr_lastname;
+                        }
+
+                        $this->username = usernameGenerator($name, $lastname, date("h:i:s"));
                         $this->password = passGenerator();
                         $this->password_hash = hash('SHA256', passGenerator());
                         $userData = array(
-                            'name' => $this->mother_name,
-                            'lastname' => $this->mother_lastname,
+                            'name' => $name,
+                            'lastname' => $lastname,
                             'username' => $this->username, 
                             'password' => $this->password_hash, 
                             'email' => $this->email
@@ -45,7 +63,7 @@
                                 $this->model->AssignUserRoles($req_id_user, $id_rol, $status);
                             }
                             $dataNewUser = array(
-                                'user' => $this->mother_name." ".$this->mother_lastname,
+                                'user' => $name." ".$lastname,
                                 'username' => $this->username,
                                 'password' => $this->password,
                                 'email' => $this->email,
@@ -54,19 +72,21 @@
                             );
 
                             $parentsData = array(
+                                'repr' => $this->repr_check,
                                 'father_name' => $this->father_name,
                                 'father_lastname' => $this->father_lastname,
                                 'mother_name' => $this->mother_name,
                                 'mother_lastname' => $this->mother_lastname,
                                 'home_phone' => $this->home_phone,
                                 'cell_phone' => $this->cell_phone,
+                                'cell_phone2' => $this->cell_phone2,
                                 'home_address' => $this->home_address,
                                 'status' => $this->status
                             ); 
                             $req = $this->model->create($req_id_user, $parentsData);
-                            $sendEmail = sendEmail($dataNewUser,'AccessCredentials');
 
                             if ($req > 0) {
+                                $sendEmail = sendEmail($dataNewUser,'AccessCredentials');
                                 if ($sendEmail) {
                                     $res = array(
                                         'status' => true, 
@@ -117,13 +137,22 @@
         public function update() {
             if ($_POST) {
                 if (verifyApiKey()) {
+                    //Id parents
                     $this->id_parents = $_POST['id_parents'];
+                    //Data personal
                     $this->father_name = $_POST['father_name'];
                     $this->father_lastname = $_POST['father_lastname'];
                     $this->mother_name = $_POST['mother_name'];
                     $this->mother_lastname = $_POST['mother_lastname'];
+                    //Representative
+                    $this->repr_check = $_POST['repr_check'];
+                    $this->repr_name = $_POST['repr_name'];
+                    $this->repr_lastname = $_POST['repr_lastname'];
+                    //Contacts
                     $this->home_phone = $_POST['home_phone'];
                     $this->cell_phone = $_POST['cell_phone'];
+                    $this->cell_phone2 = $_POST['cell_phone2'];
+                    $this->email = $_POST['email'];
                     $this->home_address = $_POST['home_address'];
                     $this->status = $_POST['status'];
 
@@ -132,26 +161,43 @@
                         || empty($this->cell_phone) || empty($this->home_address) || empty($this->status)) {
                         $res = array(
                             'status' => false, 
-                            'msg' => 'All fields are required except the password field.'
+                            'msg' => 'All fields are required.'
                         );
                     } else {
+                        if ($this->repr_check == 1) {
+                            $name = $this->father_name;
+                            $lastname = $this->father_lastname;
+                        } else if ($this->repr_check == 2) {
+                            $name = $this->mother_name;
+                            $lastname = $this->mother_lastname;
+                        } else if ($this->repr_check == 3) {
+                            $name = $this->repr_name;
+                            $lastname = $this->repr_lastname;
+                        }
+
                         $parentsData = array(
+                            'name' => $name,
+                            'lastname' => $lastname,
+                            'repr' => $this->repr_check,
                             'father_name' => $this->father_name,
                             'father_lastname' => $this->father_lastname,
                             'mother_name' => $this->mother_name,
                             'mother_lastname' => $this->mother_lastname,
+                            'email' => $this->email,
                             'home_phone' => $this->home_phone,
                             'cell_phone' => $this->cell_phone,
+                            'cell_phone2' => $this->cell_phone2,
                             'home_address' => $this->home_address,
                             'status' => $this->status
-                        ); 
+                        );
+
                         $req = $this->model->update($this->id_parents, $parentsData);
                         
                         if ($req > 0) {
                             $res = array(
                                 'status' => true, 
                                 'msg' => 'Parents updated successfully'
-                            ); 
+                            );
                         } else if ($req == 'exists') {
                             $res = array(
                                 'status' => false, 
@@ -193,6 +239,11 @@
                             $res = array(
                                 'status' => true, 
                                 'msg' => 'Parents deleted successfully.'
+                            );
+                        } else if ($req == "exists") {
+                            $res = array(
+                                'status' => false, 
+                                'msg' => 'This process could not be performed, cannot delete a parent that has patients assigned to it.'
                             );
                         } else {
                             $res = array(
