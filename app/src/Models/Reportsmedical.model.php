e3a7ed67bb491f904dc2cd3fd520bda408bd68e1
Eavila4906 <eavila4906@gmail.com>
2023-08-15 10:58:16 -0500

    Visual report of medical control

--- app/src/Models/Reportsmedical.model.php
+++ app/src/Models/Reportsmedical.model.php
@@ -10,5 +10,19 @@
                         WHERE patient = $id_patient AND status = 1"; 
             return $this->SelectAllMySQL($Query);
         }
+
+        // Unique medicalcontrol get function by appointment
+        public function getByAppointment($id_appointment) {
+            $Query = "SELECT pt.name, pt.lastname, pt.birthdate,
+                             CONCAT(us.name, ' ', us.lastname, ' - ', dc.specialty) AS doctor,
+                             ap.date, ap.photo, mc.*
+                      FROM medicalcontrol mc
+                      INNER JOIN appointment ap ON (mc.appointment=ap.id_appointment)
+                      INNER JOIN patient pt ON (ap.patient=pt.id_patient)  
+                      INNER JOIN doctor dc ON (ap.doctor=dc.id_doctor) 
+                      INNER JOIN user us ON (dc.user=us.id_user)
+                      WHERE mc.appointment = $id_appointment";
+            return $this->SelectMySQL($Query);
+        }
     }
 ?>
