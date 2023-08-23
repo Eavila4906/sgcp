0f37d50b88e9d7785b4b429dc5f7ff974f90e35f
Eavila4906 <eavila4906@gmail.com>
2023-08-22 22:05:05 -0500

    Data augmentation for displaying information for calendar events in the personal area.

--- app/src/Models/My.model.php
+++ app/src/Models/My.model.php
@@ -6,16 +6,19 @@
 
         // Appointment all get function doctor
         public function getAppointmentsDoctor($id_user) {
-            $Query = "SELECT ap.id_appointment, ap.status 
+            $Query = "SELECT ap.id_appointment, ap.status, ap.date, ap.hour, 
+                             CONCAT(pt.name, ' ', pt.lastname) AS patient
                         FROM doctor dr 
-                        INNER JOIN appointment ap ON (dr.id_doctor=ap.doctor) 
+                        INNER JOIN appointment ap ON (dr.id_doctor=ap.doctor)
+                        INNER JOIN patient pt ON (ap.patient=pt.id_patient) 
                         WHERE dr.user = $id_user AND ap.status != 0"; 
             return $this->SelectAllMySQL($Query);
         }
 
         // Appointment all get function patient
         public function getAppointmentsPatient($id_user) {
-            $Query = "SELECT ap.id_appointment, ap.status
+            $Query = "SELECT ap.id_appointment, ap.status, ap.date, ap.hour
+                             CONCAT(pt.name, ' ', pt.lastname) AS patient
                         FROM parents ps 
                         INNER JOIN patient pt ON (ps.id_parents=pt.parents)
                         INNER JOIN appointment ap ON (pt.id_patient=ap.patient)  
