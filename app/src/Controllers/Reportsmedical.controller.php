32536a9aaf71659e4b228edecc9ebc5cb1d90f4b
Eavila4906 <eavila4906@gmail.com>
2023-08-22 22:07:21 -0500

    A function was created to provide information for the generation of statistical graphs

--- app/src/Controllers/Reportsmedical.controller.php
+++ app/src/Controllers/Reportsmedical.controller.php
@@ -43,5 +43,22 @@
             die();
         }
 
+        // Unique medical control get function by charts
+        public function getRecordsByCharts() {
+            if ($_GET) {
+                if (verifyApiKey()) {
+                    $this->id_patient = $_GET['id_patient'];
+                    $res = $this->model->getRecordsByCharts($this->id_patient);
+                } else {
+                    $res = array(
+                        'status' => false,
+                        'msg' => 'Attention! You need a key to access the API.'
+                    ); 
+                } 
+                echo json_encode($res, JSON_UNESCAPED_UNICODE);
+            }
+            die();
+        }
+
     }
 ?>
